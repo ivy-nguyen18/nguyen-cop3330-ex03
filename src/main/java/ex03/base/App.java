429b4df24a09c9d7f6bb3ea06ef6612c4306147b
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-06-05 01:28:54 -0400

    Initial commit

---
+++ src/main/java/ex03/base/App.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ivy Nguyen
+ */
+
+package ex03.base;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the quote? ");
+        String quote = input.nextLine();
+
+        System.out.println("Who is the author? ");
+        String author = input.nextLine();
+
+        String quotation = author + " says," + "\""+ quote + "\"";
+        System.out.println(quotation);
+
+    }
+}
